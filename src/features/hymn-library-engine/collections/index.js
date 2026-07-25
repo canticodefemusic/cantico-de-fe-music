e2a68f904a33158fcdc8050af23bdab98ed33a0f
canticodefemusic <canticodefemusic@gmail.com>
2026-07-25 03:31:29 -0500

    Add V9.0.4 Collections public API

---
+++ src/features/hymn-library-engine/collections/index.js
@@ -0,0 +1,36 @@
+/**
+ * V9.0.4 Collections Engine
+ * Public API
+ */
+
+export {
+  CollectionService
+} from './CollectionService.js';
+
+export {
+  CollectionTemplates
+} from './CollectionTemplates.js';
+
+export {
+  CollectionRenderer
+} from './CollectionRenderer.js';
+
+export {
+  CollectionEngine
+} from './CollectionEngine.js';
+
+export {
+  default as CollectionServiceDefault
+} from './CollectionService.js';
+
+export {
+  default as CollectionTemplatesDefault
+} from './CollectionTemplates.js';
+
+export {
+  default as CollectionRendererDefault
+} from './CollectionRenderer.js';
+
+export {
+  default as CollectionEngineDefault
+} from './CollectionEngine.js';
