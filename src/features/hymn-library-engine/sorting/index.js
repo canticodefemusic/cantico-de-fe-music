c320ff99cf2ffbf2157f3c46fbde49d79130d403
canticodefemusic <canticodefemusic@gmail.com>
2026-07-25 03:54:43 -0500

    Add V9.0.5 Sort public API

---
+++ src/features/hymn-library-engine/sorting/index.js
@@ -0,0 +1,36 @@
+/**
+ * V9.0.5 Sort Engine
+ * Public API
+ */
+
+export {
+  SortService
+} from './SortService.js';
+
+export {
+  SortTemplates
+} from './SortTemplates.js';
+
+export {
+  SortRenderer
+} from './SortRenderer.js';
+
+export {
+  SortEngine
+} from './SortEngine.js';
+
+export {
+  default as SortServiceDefault
+} from './SortService.js';
+
+export {
+  default as SortTemplatesDefault
+} from './SortTemplates.js';
+
+export {
+  default as SortRendererDefault
+} from './SortRenderer.js';
+
+export {
+  default as SortEngineDefault
+} from './SortEngine.js';
