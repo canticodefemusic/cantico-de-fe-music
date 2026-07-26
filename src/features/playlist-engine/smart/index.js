66fe5ff9277c66859f381b9c905e5ff3a7440dc8
canticodefemusic <canticodefemusic@gmail.com>
2026-07-26 00:57:08 -0500

    Add V9.1.0 Smart Playlists public API

---
+++ src/features/playlist-engine/smart/index.js
@@ -0,0 +1,30 @@
+/**
+ * V9.1.0 Smart Playlists
+ * Public API
+ */
+
+export {
+  SmartPlaylistService
+} from './SmartPlaylistService.js';
+
+export {
+  SmartPlaylistDefinitions,
+  getSmartPlaylistDefinitions,
+  findSmartPlaylistDefinition
+} from './SmartPlaylistDefinitions.js';
+
+export {
+  SmartPlaylistEngine
+} from './SmartPlaylistEngine.js';
+
+export {
+  default as SmartPlaylistServiceDefault
+} from './SmartPlaylistService.js';
+
+export {
+  default as SmartPlaylistDefinitionsDefault
+} from './SmartPlaylistDefinitions.js';
+
+export {
+  default as SmartPlaylistEngineDefault
+} from './SmartPlaylistEngine.js';
