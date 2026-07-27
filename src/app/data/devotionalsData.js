187de6e0ea7976be9f89acf34087c9563707a7f5
canticodefemusic <canticodefemusic@gmail.com>
2026-07-27 02:01:47 -0500

    Create devotionals data module

---
+++ src/app/data/devotionalsData.js
@@ -0,0 +1,9 @@
+export const devotionals = [
+  {
+    id: 'fe-en-la-prueba',
+    title: 'Fe en la prueba',
+    scripture: 'Hebreos 11:1',
+    content:
+      'La fe sostiene el corazón cuando la respuesta todavía no se ve.'
+  }
+];
