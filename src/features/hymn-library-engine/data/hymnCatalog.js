ca9e18f2ed2e32dd38ecf49dfa42371122c8b9b3
canticodefemusic <canticodefemusic@gmail.com>
2026-07-28 22:14:00 -0500

    Add src property to hymn catalog

--- src/features/hymn-library-engine/data/hymnCatalog.js
+++ src/features/hymn-library-engine/data/hymnCatalog.js
@@ -15,6 +15,7 @@ export const hymnCatalog = [
     artist: 'Cántico de Fe Music',
 
     audio: '/assets/audio/himnos/fe-que-mueve-montanas.mp3',
+    src: '/assets/audio/himnos/fe-que-mueve-montanas.mp3',
     cover: '',
     duration: '',
 
@@ -61,6 +62,7 @@ export const hymnCatalog = [
     artist: 'Cántico de Fe Music',
 
     audio: '/assets/audio/himnos/quince-anos-mas.m4a',
+    src: '/assets/audio/himnos/quince-anos-mas.m4a',
     cover: '',
     duration: '',
 
