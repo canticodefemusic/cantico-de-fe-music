da4751f43294df8f050a312e75f64e6b734c565d
canticodefemusic <canticodefemusic@gmail.com>
2026-08-02 03:27:02 -0500

    V10.7 Add Modal Engine API

---
+++ src/features/modal-engine/index.js
@@ -0,0 +1,10 @@
+/**
+ * Cántico de Fe Music
+ * V10.7 Modal Engine
+ */
+
+export { ModalService }
+  from './services/ModalService.js';
+
+export { renderModal }
+  from './components/renderModal.js';
