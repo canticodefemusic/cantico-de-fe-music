a3ba82825d0cdaf3f6749cfce28ad394465b6e83
canticodefemusic <canticodefemusic@gmail.com>
2026-08-03 23:52:53 -0500

    V11.1 Create Global Search Public API

---
+++ src/features/global-search-engine/index.js
@@ -0,0 +1,14 @@
+/**
+ * Cántico de Fe Music
+ * V11.1 — Global Search Engine
+ * Public API
+ */
+
+import GlobalSearchService
+  from './services/GlobalSearchService.js';
+
+export {
+  GlobalSearchService
+};
+
+export default GlobalSearchService;
