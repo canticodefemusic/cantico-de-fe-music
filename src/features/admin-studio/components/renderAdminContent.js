44ca99b8fd82704db0385f524e45df8527bd7979
canticodefemusic <canticodefemusic@gmail.com>
2026-08-04 04:27:43 -0500

    V12.4 Add Export Actions UI

--- src/features/admin-studio/components/renderAdminContent.js
+++ src/features/admin-studio/components/renderAdminContent.js
@@ -429,21 +429,39 @@ function renderHymnManager() {
           </p>
         </div>
 
-        <button
-          type="button"
-          class="admin-section__primary-action"
-          data-admin-create="hymns"
-        >
-          <span
-            aria-hidden="true"
-          >
-            +
-          </span>
+        <div
+  class="admin-section__actions"
+>
+  <button
+    type="button"
+    data-admin-hymn-export
+  >
+    Exportar catálogo
+  </button>
 
-          <span>
-            Nuevo himno
-          </span>
-        </button>
+  <button
+    type="button"
+    data-admin-hymn-backup
+  >
+    Descargar respaldo
+  </button>
+
+  <button
+    type="button"
+    class="admin-section__primary-action"
+    data-admin-create="hymns"
+  >
+    <span
+      aria-hidden="true"
+    >
+      +
+    </span>
+
+    <span>
+      Nuevo himno
+    </span>
+  </button>
+</div>
       </header>
 
       ${renderHymnStats()}
