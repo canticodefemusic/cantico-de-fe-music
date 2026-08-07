b902b4b628548630e2ddff1b36f4434fd7ea010f
canticodefemusic <canticodefemusic@gmail.com>
2026-08-07 03:31:10 -0500

    V13.1.2 Update module exports

--- src/features/universal-upload-engine/index.js
+++ src/features/universal-upload-engine/index.js
@@ -1,13 +1,14 @@
 export * from './UploadEngine.js';
 
 export * from './services/UploadService.js';
-
+export * from './services/UploadQueueService.js';
 export * from './services/DragDropService.js';
 
 export * from './state/uploadState.js';
 
 export * from './utils/uploadValidator.js';
-
 export * from './utils/fileTypeDetector.js';
+export * from './utils/createUploadItem.js';
 
 export * from './constants/uploadConfig.js';
+export * from './constants/uploadStatus.js';
