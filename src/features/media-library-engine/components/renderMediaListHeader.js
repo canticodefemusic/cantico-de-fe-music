dd5e38283398a55203013175f4cf985e7df3e159
canticodefemusic <canticodefemusic@gmail.com>
2026-08-09 01:41:58 -0500

    V13.10.2 Create Apple Music list header component

---
+++ src/features/media-library-engine/components/renderMediaListHeader.js
@@ -0,0 +1,24 @@
+export function renderMediaListHeader() {
+
+  return `
+    <div
+      class="media-list-header"
+      role="row"
+    >
+
+      <div></div>
+
+      <div>Nombre</div>
+
+      <div>Tipo</div>
+
+      <div>Tamaño</div>
+
+      <div>Fecha</div>
+
+      <div></div>
+
+    </div>
+  `;
+
+}
